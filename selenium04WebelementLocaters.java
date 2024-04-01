1a680ec33d9ac347f82424724d79e24f487fb4af
Seval <aslanseval1986@gmail.com>
2024-04-01 15:18:28 +0100

    selenium04_WebElements_Locaters

---
+++ selenium04WebelementLocaters.java
@@ -0,0 +1,41 @@
+package selenuim;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class selenium04WebelementLocaters {
+    @Test
+    public void locators() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        //amazon sitesine gidilir
+        driver.get("https://www.amazon.com/"); //istenen sayfaya gider
+
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+
+        WebElement id_SearchBox= driver.findElement(By.id("twotabsearchtextbox"));
+        id_SearchBox.click();
+
+        WebElement xpath_SearchBox= driver.findElement(By.xpath("//input[@placeholder='Search Amazon']"));
+
+        WebElement cssSelector_English= driver.findElement(By.cssSelector("span[class='icp-nav-link-inner']"));
+
+        WebElement name_SearchBox= driver.findElement(By.name("field-keywords"));
+
+        WebElement className_SearchBox=driver.findElement(By.className("nav-progressive-attribute"));
+
+        WebElement tagName_Header= driver.findElement(By.tagName("header"));
+
+        WebElement linkText_English= driver.findElement(By.linkText("EN"));
+        linkText_English.click();
+
+        WebElement partialLinkText_English=driver.findElement(By.partialLinkText("Hello, sign in"));
+        Thread.sleep(2000);
+
+        driver.close();
+    }
+}
